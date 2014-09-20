1689503e81140a31f3c2b2a057a1a432c4c66cca
blackeye6 <masterman2@live.com>
2014-09-20 12:49:02 -0400

    Add trigger words

--- src/main/java/me/xTDKx/CB/Listeners/AsyncChat.java
+++ src/main/java/me/xTDKx/CB/Listeners/AsyncChat.java
@@ -9,8 +9,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.HashMap;
+
 public class AsyncChat implements Listener {
     private ChatterBot plugin;
+    private HashMap<String, Long> triggered = new HashMap<String, Long>();
+    private HashMap<String, ChatterBotSession> sessions = new HashMap<String, ChatterBotSession>();
 
     public AsyncChat(ChatterBot p) {
         plugin = p;
@@ -19,28 +23,109 @@ public class AsyncChat implements Listener {
     @EventHandler
     public void onChat(final AsyncPlayerChatEvent e) {
         if (!e.isCancelled()) {
-                if (CBAssign.assignie.containsKey(e.getPlayer().getName())) {
-                    final ChatterBotSession cbSession = CBAssign.assignie.get(e.getPlayer().getName());
+            if (CBAssign.assignie.containsKey(e.getPlayer().getName())) {
+                final ChatterBotSession cbSession = CBAssign.assignie.get(e.getPlayer().getName());
 
-                    Bukkit.getScheduler().runTaskAsynchronously(ChatterBot.instance, new Runnable() {
-                        @Override
-                        public void run() {
-                            final StringBuilder sb = new StringBuilder();
-                            try {
-                                sb.append(cbSession.think(e.getMessage()));
-                            } catch (Exception e) {
-                                e.printStackTrace();
+                Bukkit.getScheduler().runTaskAsynchronously(ChatterBot.instance, new Runnable() {
+                    @Override
+                    public void run() {
+                        final StringBuilder sb = new StringBuilder();
+                        try {
+                            sb.append(cbSession.think(e.getMessage()));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        Bukkit.getScheduler().runTask(ChatterBot.instance, new Runnable() {
+                            @Override
+                            public void run() {
+                                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ChatterBot-Format").replace("%name%", ChatterBot.chatterBotName).replace("%message%", sb.toString())));
                             }
+                        });
+                    }
+                });
+            } else {
+                for (String word : ChatterBot.instance.getConfig().getStringList("Trigger-Word-List")) {
+                    if (e.getMessage().toLowerCase().contains(word.toLowerCase())) {
+                        if (triggered.containsKey(e.getPlayer().getName())) {
+                            triggered.put(e.getPlayer().getName(), System.currentTimeMillis() + (ChatterBot.instance.getConfig().getInt("Trigger-Word-Timeout") * 1000));
 
-                            Bukkit.getScheduler().runTask(ChatterBot.instance, new Runnable() {
+                            final ChatterBotSession cbSession = sessions.get(e.getPlayer().getName());
+
+                            Bukkit.getScheduler().runTaskAsynchronously(ChatterBot.instance, new Runnable() {
                                 @Override
                                 public void run() {
-                                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ChatterBot-Format").replace("%bot%", ChatterBot.chatterBotName).replace("%message%", sb.toString())));
+                                    final StringBuilder sb = new StringBuilder();
+                                    try {
+                                        sb.append(cbSession.think(e.getMessage()));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    Bukkit.getScheduler().runTask(ChatterBot.instance, new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ChatterBot-Format").replace("%name%", ChatterBot.chatterBotName).replace("%message%", sb.toString())));
+                                        }
+                                    });
+                                }
+                            });
+                        } else {
+                            final ChatterBotSession cbSession = ChatterBot.bot1.createSession();
+
+                            triggered.put(e.getPlayer().getName(), System.currentTimeMillis() + (ChatterBot.instance.getConfig().getInt("Trigger-Word-Timeout") * 1000));
+                            sessions.put(e.getPlayer().getName(), cbSession);
+
+                            Bukkit.getScheduler().runTaskAsynchronously(ChatterBot.instance, new Runnable() {
+                                @Override
+                                public void run() {
+                                    final StringBuilder sb = new StringBuilder();
+                                    try {
+                                        sb.append(cbSession.think(e.getMessage()));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    Bukkit.getScheduler().runTask(ChatterBot.instance, new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ChatterBot-Format").replace("%name%", ChatterBot.chatterBotName).replace("%message%", sb.toString())));
+                                        }
+                                    });
                                 }
                             });
                         }
-                    });
+
+                        return;
+                    }
                 }
+
+                if (triggered.containsKey(e.getPlayer().getName())) {
+                    if (!(System.currentTimeMillis() >= triggered.get(e.getPlayer().getName()))){
+
+                        final ChatterBotSession cbSession = sessions.get(e.getPlayer().getName());
+
+                        Bukkit.getScheduler().runTaskAsynchronously(ChatterBot.instance, new Runnable() {
+                            @Override
+                            public void run() {
+                                final StringBuilder sb = new StringBuilder();
+                                try {
+                                    sb.append(cbSession.think(e.getMessage()));
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+
+                                Bukkit.getScheduler().runTask(ChatterBot.instance, new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("ChatterBot-Format").replace("%name%", ChatterBot.chatterBotName).replace("%message%", sb.toString())));
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }
+            }
         }
     }
 
